30bcc5d348eb72ceb964d8d7d8b62d7a2e0904f5
TheNormalized <82162764+TheNormalized@users.noreply.github.com>
2023-05-10 20:52:51 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# -
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-r4w1ok)
---
+++ src/views/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+import { useHomeStore } from '../store';
+const home = useHomeStore();
+</script>
+<template>
+  <div>{{ home.name }}</div>
+</template>
